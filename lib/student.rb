feed8cf05c43dcedd0447b6ef19fd0585f36208b
johnshelb <jsnd11@gmail.com>
2017-02-09 20:38:11 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge=[]
+  end
+
+  def learn(knowstring)
+    @knowledge<<knowstring
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
